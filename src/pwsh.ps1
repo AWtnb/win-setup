e26836177be158379728f9587b46bb404acd6016
awtnb <arikaebanataw@gmail.com>
2023-06-29 21:25:48 +0900

    sync pwsh with sycthing

--- src/pwsh.ps1
+++ src/pwsh.ps1
@@ -1,4 +1,4 @@
 . ($PSScriptRoot | Join-Path -ChildPath "_funcs.ps1")
 
-Invoke-MySetup -workDir ($env:USERPROFILE | Join-Path -ChildPath "Documents") -src ($env:USERPROFILE | Join-Path -ChildPath "Dropbox\develop\app_config\PowerShell\PowerShell")
+Invoke-MySetup -workDir ($env:USERPROFILE | Join-Path -ChildPath "Documents") -src ($env:USERPROFILE | Join-Path -ChildPath "Sync\develop\repo\PowerShell")
 
