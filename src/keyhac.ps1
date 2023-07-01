4073e0380d80f6af75efa5c3e5ef0f90aaaa3563
awtnb <arikaebanataw@gmail.com>
2023-07-01 20:46:46 +0900

    update

--- src/keyhac.ps1
+++ src/keyhac.ps1
@@ -2,8 +2,8 @@
 . ($PSScriptRoot | Join-Path -ChildPath "_funcs.ps1")
 
 @{
-    "C:\Personal\tools\keyhac" = "repo\keyhac\config.py";
-    "C:\Personal\tools\keyhac\theme\black" = "repo\keyhac\theme.ini";
+    "C:\Personal\tools\keyhac" = "Sync\develop\repo\keyhac\config.py";
+    "C:\Personal\tools\keyhac\theme\black" = "Sync\develop\repo\keyhac\theme.ini";
 }.GetEnumerator() | ForEach-Object {
     $src = $env:USERPROFILE | Join-Path -ChildPath $_.Value
     Invoke-MySetup -workDir $_.Key -src $src
