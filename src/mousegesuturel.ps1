fe11cf2ef95711a3feb9b9ab13b443d241f7b939
awtnb <arikaebanataw@gmail.com>
2023-07-07 19:30:23 +0900

    change sync path

--- src/mousegesuturel.ps1
+++ src/mousegesuturel.ps1
@@ -1,3 +1,3 @@
 . ($PSScriptRoot | Join-Path -ChildPath "_funcs.ps1")
 
-New-ShortCutOnStartup -path ($env:USERPROFILE | Join-Path -ChildPath "Dropbox\portable_apps\MGLahk138\MouseGestureL.exe")
+New-ShortCutOnStartup -path ($env:USERPROFILE | Join-Path -ChildPath "Sync\portable_app\MGL\MouseGestureL.exe")
