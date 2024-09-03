ba539bbe6fdd235dca55c731d22fb5728c3f3837
awtnb <arikaebanataw@gmail.com>
2024-09-03 11:34:58 +0900

    sync wezterm on its way

--- src/wezterm.ps1
+++
@@ -1,4 +0,0 @@
-. ($PSScriptRoot | Join-Path -ChildPath "_funcs.ps1")
-
-Invoke-MySetup -workDir ($env:USERPROFILE | Join-Path -ChildPath ".config") -src ($env:USERPROFILE | Join-Path -ChildPath "Sync\develop\repo\wezterm")
-
